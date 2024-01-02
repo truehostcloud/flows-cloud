590da151c5aebdf9ec79989c116a3ad4a52c2bcd
David Pešička <61206880+pesickadavid@users.noreply.github.com>
2024-01-02 21:51:37 +0100

    fix(app): login

--- apps/app/src/lib/constants.ts
+++ apps/app/src/lib/constants.ts
@@ -5,3 +5,4 @@ export const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const PRODUCTION = process.env.NODE_ENV === "production";
+export const DEVELOPMENT = process.env.NODE_ENV === "development";
