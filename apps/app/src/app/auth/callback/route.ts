fcfce8501b83d733ff128f1adf3578c2e2298943
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-01-03 09:15:33 +0100

    fix(app): login

--- apps/app/src/app/auth/callback/route.ts
+++ apps/app/src/app/auth/callback/route.ts
@@ -1,5 +1,5 @@
 import { createClient } from "auth/server";
-import { cookies } from "next/headers";
+import { cookies, headers } from "next/headers";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 import { routes } from "routes";
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the Auth Helpers package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
-  const requestUrl = request.nextUrl.clone();
+  const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get("code");
 
   if (code) {
@@ -16,7 +16,10 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  requestUrl.pathname = routes.home;
+  const origin = headers().get("x-forwarded-host");
+  const protocol = headers().get("x-forwarded-proto");
+  const redirectTo = `${protocol}://${origin}${routes.home}`;
+
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl);
+  return NextResponse.redirect(redirectTo);
 }
