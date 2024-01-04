158c40bd40983c661ef237a6afb94c496735173e
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-01-04 07:42:02 +0100

    fix(app): supabase middleware

--- apps/app/src/middleware.ts
+++ apps/app/src/middleware.ts
@@ -10,18 +10,7 @@ export async function middleware(request: NextRequest): Promise<NextResponse> {
 
   // Refresh session if expired - required for Server Components
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-session-with-middleware
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  const sessionCookies = {
-    name: "sb-admin-auth-token",
-    value: JSON.stringify(session),
-    path: "/",
-    expires: new Date(new Date().getTime() + 60 * 60 * 1000 * 24 * 365), // 1 year,
-  };
-
-  response.cookies.set(sessionCookies);
+  await supabase.auth.getSession();
 
   return response;
 }
