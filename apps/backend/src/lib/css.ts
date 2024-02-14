5fbcbcc3a88bc1489c524210f60589a2749a4138
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-02-14 09:20:32 +0100

    fix(be): css version vulnerability

--- apps/backend/src/lib/css.ts
+++ apps/backend/src/lib/css.ts
@@ -1,9 +1,16 @@
+const verifyVersion = (version: string): string => {
+  if (version === "latest") return version;
+  const isSemVer = /^\d+\.\d+\.\d+$/.test(version);
+  if (isSemVer) return version;
+  return "latest";
+};
+
 export const getDefaultCssVars = (version = "latest"): Promise<string> =>
-  fetch(`https://cdn.jsdelivr.net/npm/@flows/js@${version}/css.min/vars.css`).then((res) =>
-    res.text(),
+  fetch(`https://cdn.jsdelivr.net/npm/@flows/js@${verifyVersion(version)}/css.min/vars.css`).then(
+    (res) => res.text(),
   );
 
 export const getDefaultCssTemplate = (version = "latest"): Promise<string> =>
-  fetch(`https://cdn.jsdelivr.net/npm/@flows/js@${version}/css.min/template.css`).then((res) =>
-    res.text(),
-  );
+  fetch(
+    `https://cdn.jsdelivr.net/npm/@flows/js@${verifyVersion(version)}/css.min/template.css`,
+  ).then((res) => res.text());
