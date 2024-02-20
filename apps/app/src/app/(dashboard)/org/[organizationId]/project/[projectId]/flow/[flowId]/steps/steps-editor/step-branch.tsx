63fc87e17ad4a9fc20f8cd8dc43d6940b2c7491a
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-02-20 11:46:26 +0100

    feat(app): insert step in branch (#100)
    closes #95

--- apps/app/src/app/(dashboard)/org/[organizationId]/project/[projectId]/flow/[flowId]/steps/steps-editor/step-branch.tsx
+++ apps/app/src/app/(dashboard)/org/[organizationId]/project/[projectId]/flow/[flowId]/steps/steps-editor/step-branch.tsx
@@ -1,9 +1,11 @@
-import { Box, Flex } from "@flows/styled-system/jsx";
+import { css } from "@flows/styled-system/css";
+import { Flex } from "@flows/styled-system/jsx";
 import { Plus16 } from "icons";
-import { type FC } from "react";
+import { type FC, Fragment } from "react";
 import { type Control, useFieldArray } from "react-hook-form";
 import { Accordion, Button, Text } from "ui";
 
+import { InsertButton } from "./insert-button";
 import { STEP_DEFAULT, StepForm } from "./step-form";
 import type { StepsForm } from "./steps-editor.types";
 
@@ -14,14 +16,14 @@ type Props = {
 };
 
 export const StepBranch: FC<Props> = ({ control, index, onRemove }) => {
-  const { append, remove, fields } = useFieldArray({ control, name: `steps.${index}` });
+  const { append, remove, fields, insert } = useFieldArray({ control, name: `steps.${index}` });
   const branchIndex = index.split(".")[1];
 
   return (
     <Accordion
       title={
         <Flex alignItems="center" mr="space8">
-          <Flex flex={1} gap="space8">
+          <Flex alignItems="center" flex={1} gap="space8">
             <Text as="span" variant="titleM">
               Branch
             </Text>
@@ -42,11 +44,18 @@ export const StepBranch: FC<Props> = ({ control, index, onRemove }) => {
       }
     >
       {fields.map((field, i) => (
-        <Box key={field.id} pb="space12">
+        <Fragment key={field.id}>
+          <InsertButton
+            className={css({ mt: i === 0 ? "-space16" : 0 })}
+            onClick={() => insert(i, STEP_DEFAULT.tooltip)}
+          >
+            Insert step
+          </InsertButton>
           <StepForm control={control} index={`${index}.${i}`} onRemove={() => remove(i)} />
-        </Box>
+        </Fragment>
       ))}
       <Button
+        className={css({ mt: "space16" })}
         onClick={() => append(STEP_DEFAULT.tooltip)}
         shadow={false}
         startIcon={<Plus16 />}
