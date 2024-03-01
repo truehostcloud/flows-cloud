ccec7f2d20a20031ec8bfa4c81b8664db6933e41
Vojtěch Vidra <vojtechvidra@gmail.com>
2024-03-01 12:53:20 +0100

    feat(app): server api url (#132)

--- apps/app/src/index.d.ts
+++ apps/app/src/index.d.ts
@@ -1,6 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      SERVER_API_URL?: string;
       NEXT_PUBLIC_SUPABASE_URL: string;
       NEXT_PUBLIC_SUPABASE_ANON_KEY: string;
       NEXT_PUBLIC_API_URL: string;
